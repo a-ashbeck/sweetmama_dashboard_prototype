51278fc2a315216abd9ce6aaa2093daa4fc79ca8
Alan <a-ashbeck@northwestern.edu>
2016-08-17 14:43:30 -0500

    Add messages

---
+++ app/controllers/prototype/landings_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Prototype
+  # Static prototype messages controller
+  class LandingsController < ::DashboardController
+    def show
+    end
+  end
+end
---
+++ app/controllers/prototype/messages_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Prototype
+  # Static prototype messages controller
+  class MessagesController < ::DashboardController
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+  end
+end
